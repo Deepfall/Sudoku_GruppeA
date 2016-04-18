a4ff70f88faabf26d2db49cc6bdd0aac502afeea
Deepfall <da.e.fischer@googlemail.com>
2016-04-18 10:42:22 +0200

    change tabs to space

--- Sudoku_GruppeA_18042016/main.c
+++ Sudoku_GruppeA_18042016/main.c
@@ -28,7 +28,7 @@ Praeprozessoranweisungen
 /*******************************************************************************
 Funktionsprototypen
 *******************************************************************************/
-
+void getFormattedTime(char formattedTime[9], int elapsedSeconds);
 
 /*******************************************************************************
 Globale Variablen
@@ -38,91 +38,90 @@ Globale Variablen
 /*******************************************************************************
 Funktion main()
 *******************************************************************************/
-void getFormattedTime(char formattedTime[9], int elapsedSeconds)
-{
-	int hour = elapsedSeconds / 3600;
-	int minutes = (elapsedSeconds / 60) % 60;
-	int seconds = elapsedSeconds % 60;
-	
-	sprintf(formattedTime, "%02d:%02d:%02d", hour, minutes, seconds);
-}
-
 int main(void)
 {
-	int elapsedSeconds, pressedKey;
-	char formattedElapsedTime[9];
-	time_t startTime, currentTime;
-	
+    int elapsedSeconds, pressedKey;
+    char formattedElapsedTime[9];
+    time_t startTime, currentTime;
+
     system("MODE CON: COLS=160");
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-	timeout(1000);
-	time(&startTime);
-	
-	while(1) {
-		pressedKey = getch();
-	
-		clear();
-		time(&currentTime);
-		elapsedSeconds = (int) difftime(currentTime, startTime);
-		getFormattedTime(formattedElapsedTime, elapsedSeconds);
-		
-		printw("      A       B       C        D       E       F        G       H      I                                      \n");
-		printw("  +-------+-------+----------------+-------+----------------+-------+-------+                                 \n");
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    timeout(500);
+    time(&startTime);
+
+    while(1) {
+        pressedKey = getch();
+
+        clear();
+        time(&currentTime);
+        elapsedSeconds = (int) difftime(currentTime, startTime);
+        getFormattedTime(formattedElapsedTime, elapsedSeconds);
+
+        printw("      A       B       C        D       E       F        G       H      I                                      \n");
+        printw("  +-------+-------+----------------+-------+----------------+-------+-------+                                 \n");
         printw("  |       |       |       ||       |       |       ||       |       |       |     Zeit:          %s     \n", formattedElapsedTime);
-		printw("1 |       |       |       ||       |       |       ||       |       |       |     Hilfe genutzt: 0            \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+     [H] Hilfe (Feld füllen)     \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |     [K] Kandidaten anzeigen     \n");
-		printw("2 |       |       |       ||       |       |       ||       |       |       |     [L] Lösung                  \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |     [R] Spielregeln             \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("3 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |=========================================================================|                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("4 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("5 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("6 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |=========================================================================|                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("7 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("8 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("9 |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
-		printw("  +-------+-------+----------------+-------+----------------+-------+-------+                                 \n");
-		
-		printw("Pressed key: %d", pressedKey);
+        printw("1 |       |       |       ||       |       |       ||       |       |       |     Hilfe genutzt: 0            \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+     [H] Hilfe (Feld füllen)     \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |     [K] Kandidaten anzeigen     \n");
+        printw("2 |       |       |       ||       |       |       ||       |       |       |     [L] Lösung                  \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |     [R] Spielregeln             \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("3 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |=========================================================================|                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("4 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("5 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("6 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |=========================================================================|                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("7 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("8 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+-------++-------+-------+-------++-------+-------+-------+                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("9 |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  |       |       |       ||       |       |       ||       |       |       |                                 \n");
+        printw("  +-------+-------+----------------+-------+----------------+-------+-------+                                 \n");
+
+        printw("Pressed key: %d", pressedKey);
 
         move(10, 10);
-		
-		refresh();
-	}
-	
-	endwin();
-	
-	return OK;
+
+        refresh();
+    }
+
+    endwin();
+
+    return OK;
 }
 
 /*******************************************************************************
-Funktion ()
-Uebergabe Parameter:    -
+Funktion getFormattedTime()
+Uebergabe Parameter:    formattedTime[9], elapsedSeconds
 Rueckgabe:              void
 Beschreibung:           
 *******************************************************************************/
+void getFormattedTime(char formattedTime[9], int elapsedSeconds)
+{
+    int hour = elapsedSeconds / 3600;
+    int minutes = (elapsedSeconds / 60) % 60;
+    int seconds = elapsedSeconds % 60;
+
+    sprintf(formattedTime, "%02d:%02d:%02d", hour, minutes, seconds);
+}
